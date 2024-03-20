90ffcb72f8d51dad0317fe40952f2b9ce5d3a8a6
t4vzz <souzza.otavio@gmail.com>
2024-03-20 20:29:17 -0300

    first commit

---
+++ src/main/java/com/eteczl/otavio1/aplicacao.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eteczl.otavio1;
+
+/**
+ *
+ * @author Admin
+ */
+public class aplicacao {
+    
+    public static void main(String args[]){
+            System.out.println(" Tênis da Nike! "); 
+        
+            Tenis nike1 = new Tenis();
+            Tenis nike2 = new Tenis(); 
+            Tenis nike3 = new Tenis();
+            
+            nike1.modelo = ("AirMax 90");
+            nike2.modelo = ("AirMax TN Plus");
+            nike3.modelo = ("AirMax TN 3 ");
+            
+            Tenis.Marca = ("Nike");
+            
+            
+    
+    }   
+}
---
+++ src/main/java/com/eteczl/otavio1/tenis.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eteczl.otavio1;
+
+/**
+ *
+ * @author Admin
+ */
+class Tenis {
+    
+
+public String modelo;
+public static String Marca;
+      
+    
+}
